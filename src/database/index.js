e5679b298578cddc2379d918a07b99167e40e6bf
Ian Miranda <miranda.dev43@gmail.com>
2021-07-23 14:42:25 -0300

    Add 'dotenv' and 'mongoose' libs, create an '.env' file and make the DB connection

---
+++ src/database/index.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose'
+
+mongoose.connect(process.env.DB_CONNECTION_STRING_DEV, {useNewUrlParser: true, useUnifiedTopology: true});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Database connected successfully')
+});
+
+export default mongoose;
--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
+import 'dotenv/config';
 import express from 'express';
 
 const app = express();
-const port = 3333;
+const port = 3000;
 
 app.use(express.json());
+
+app.get('/', (req, res) => res.json('Hello world!!!'));
 
 app.listen(port, () => console.log(`Server is running at port ${port}`));
